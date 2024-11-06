18fd6b7ad1978d412ce08206b725a56679821845
mdarrakibislamrifat <rohanrifat43@gmail.com>
2024-11-06 13:41:15 +0600

    Access modifier private,public,protected

---
+++ module3/3.5.ts
@@ -0,0 +1,29 @@
+{
+  //
+
+  class BankAccount {
+    readonly id: number;
+    public name: string;
+    private _balance: number;
+
+    constructor(id: number, name: string, _balance: number) {
+      this.id = id;
+      this.name = name;
+      this._balance = _balance;
+    }
+    depositBalance(amount: number) {
+      this._balance = this._balance + amount;
+    }
+    getBalance() {
+      return this._balance;
+    }
+  }
+
+  const goribManusAccount = new BankAccount(111, "Mr.Gorib", 20);
+
+  goribManusAccount.depositBalance(20);
+  const myBalance = goribManusAccount.getBalance();
+  console.log(myBalance);
+
+  //
+}
